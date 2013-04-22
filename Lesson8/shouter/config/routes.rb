90ef0c32bbe88ea0eb9423a158d5c0e722fab42a
saifeldeen <saifeldeen@gmail.com>
2013-04-22 20:17:38 +1000

    routine commit

--- Lesson8/shouter/config/routes.rb
+++ Lesson8/shouter/config/routes.rb
@@ -1,7 +1,7 @@
 Shouter::Application.routes.draw do
   get '/shout' => 'shout#index', :as => 'shout'
 
-  get '/' => 'shout#index'
+  get '/' => 'shout#index', :as => 'home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
